c9f764fab998dc546312f2d03a8d533e21b11430
sashagod1986 <sashagod1986@gmail.com>
2022-06-21 02:00:28 +0300

    first-day-end

--- task-for-courses/src/main/java/org/example/Main.java
+++ task-for-courses/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +11,30 @@ public class Main {
         String arithmetic = ArithmeticObj.nextLine();
         System.out.println("arithmetic="+arithmetic);
 
+        int a=Character.getNumericValue(arithmetic.charAt(0));
+        System.out.println("first="+a);
+        System.out.println("lenght="+arithmetic.length());
+        int slenght = arithmetic.length();
+        System.out.println("slenght="+slenght);
+        System.out.println("intarrey=");
+        char chararrey[] = new char[slenght];
+        for (int i=0;i<slenght;i++){
+            if (arithmetic.charAt(i) != '+' && arithmetic.charAt(i) != '*' && arithmetic.charAt(i) != '/' && arithmetic.charAt(i) != '(' && arithmetic.charAt(i) != ')'){
+
+                chararrey[i]=arithmetic.charAt(i);
+
+            }
+            else chararrey[i]='|';
+        }
+        System.out.print(Arrays.toString(chararrey));
+        int numbarrey[] = new int[slenght];
+        System.out.println("digit=");
+        for (int i=0;i<slenght;i++){
+            if (chararrey[i] == '-' || chararrey[i] == '|') {
+                System.out.print("i=" + i + ",");
+            }
+        }
+
 
     }
-
 }
