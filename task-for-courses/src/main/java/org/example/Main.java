4f581920c5784bb8eb92525fff08d39fbc76164d
sashagod1986 <107869580+sashagod1986@users.noreply.github.com>
2022-06-20 22:45:49 +0300

    Update Main.java

--- task-for-courses/src/main/java/org/example/Main.java
+++ task-for-courses/src/main/java/org/example/Main.java
@@ -12,8 +12,8 @@ public class Main {
         System.out.println("arithmetic="+arithmetic);
         System.out.println("arithmetic="+arithmetic);
         System.out.println("arithmetic="+arithmetic);
-
         System.out.println("arithmetic="+arithmetic);
+        
 
     }
 
