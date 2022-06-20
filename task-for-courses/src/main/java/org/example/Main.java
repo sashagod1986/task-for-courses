2481cdf1d646a2187f166efc10ea302cd5341bcb
sashagod1986 <sashagod1986@gmail.com>
2022-06-20 18:48:17 +0300

    initial project version

---
+++ task-for-courses/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ArithmeticObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter arithmetic expression");
+        System.out.println("new courses");
+        String arithmetic = ArithmeticObj.nextLine();
+        System.out.println("arithmetic="+arithmetic);
+        
+    }
+
+}
