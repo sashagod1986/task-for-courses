ad34589032a713504ad61d9af840de2333824d4b
sashagod1986 <sashagod1986@gmail.com>
2022-06-25 01:26:53 +0300

    bez skobok

--- task-for-courses/src/main/java/org/example/Main.java
+++ task-for-courses/src/main/java/org/example/Main.java
@@ -7,6 +7,32 @@ import java.util.Scanner;
 
 
 public class Main {
+    public static char[] removeArrayElementChar(char[] needarray, int index){
+            StringBuilder sb = new StringBuilder();
+            sb.append(needarray);
+            sb.deleteCharAt(index);
+            return sb.toString().toCharArray();
+    }
+    public static float[] removeArrayElementFloat(float[] needarray, int index){
+        float[] tempArray = new float[needarray.length-1];
+        for (int i = 0, k = 0;i<needarray.length; i++) {
+            if (i == index) {
+                continue;
+            } else
+                tempArray[k++] = needarray[i];
+        }
+        return tempArray;
+    }
+
+    public static boolean ifHaveElement(char[] needarray, char element0, char element1){
+        boolean result = false;
+        for (int i=0;i<needarray.length;i++) {
+            if (needarray[i] == element0 || needarray[i] == element1) result = true;
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         Scanner ArithmeticObj = new Scanner(System.in); // Create a Scanner object
         System.out.println("Enter arithmetic expression");
@@ -22,6 +48,7 @@ public class Main {
         if (brackets < 0) System.out.println("extra parenthesis )");
         if (brackets == 0) System.out.println("brackets ok");
 
+
         char[] arithmetichar = arithmetic.toCharArray();
 
         boolean arithmeticsigns = true;
@@ -33,6 +60,13 @@ public class Main {
                 arithmeticsigns = false;
             }
         }
+        if ((arithmetic.charAt(0) == '+' || arithmetic.charAt(0) == '*' || arithmetic.charAt(0) == '/') && (arithmetic.charAt(1) == '+' || arithmetic.charAt(1) == '-' || arithmetic.charAt(1) == '*' || arithmetic.charAt(1) == '/')){
+            arithmeticsigns = false;
+        }
+        if (arithmetic.charAt(arithmetic.length()-1) == '+' || arithmetic.charAt(arithmetic.length()-1) == '-' || arithmetic.charAt(arithmetic.length()-1) == '*' || arithmetic.charAt(arithmetic.length()-1) == '/'){
+            arithmeticsigns = false;
+        }
+
         if (arithmeticsigns) {
             System.out.println("arithmetic signs ok");
 
@@ -58,7 +92,7 @@ public class Main {
 
             float[] floatdigit = new float[digits.length];
             for(int i=0;i<floatdigit.length;i++){
-                floatdigit[i] = Float.valueOf(digits[i]);
+                floatdigit[i] = Float.parseFloat(digits[i]);
                 System.out.print(" float="+floatdigit[i]+" ");
             }
             System.out.println();
@@ -78,14 +112,62 @@ public class Main {
             }
 
 
-
             for (int i=0;i<arithmetikchar.length;i++){
                 System.out.print(" arithmetic="+arithmetikchar[i]+" ");
             }
             System.out.println();
-            System.out.print(" arithmetic lenght="+arithmeticsig+" ");
+            System.out.println(" arithmetic lenght="+arithmetikchar.length+" ");
+
+
+            if (ifHaveElement(arithmetikchar, '*', '/'))  System.out.println(" arithmetikchar have * or /");
+            while ((ifHaveElement(arithmetikchar, '*', '/')) && (arithmetikchar.length > 0) ){
+                for (int i = 0; i < arithmetikchar.length; i++) {
+                    if (arithmetikchar[i] == '*' || arithmetikchar[i] == '/') {
+                        if (arithmetikchar[i] == '*'){
+                            floatdigit[i]=floatdigit[i]*floatdigit[i+1];
+                            System.out.print(" float "+i+" ="+floatdigit[i]+" ");
+                        }
+                        if (arithmetikchar[i] == '/'){
+                            floatdigit[i]=floatdigit[i]/floatdigit[i+1];
+                            System.out.print(" float "+i+" ="+floatdigit[i]+" ");
+                        }
+                        arithmetikchar = removeArrayElementChar(arithmetikchar,i);
+                        floatdigit = removeArrayElementFloat(floatdigit,i+1);
+                    }
+                }
+
+            }
+            while (arithmetikchar.length > 0){
+                for (int i = 0; i < arithmetikchar.length; i++) {
+                    if (arithmetikchar[i] == '+' || arithmetikchar[i] == '-') {
+                        if (arithmetikchar[i] == '+'){
+                            floatdigit[i]=floatdigit[i]+floatdigit[i+1];
+                            System.out.print(" float "+i+" ="+floatdigit[i]+" ");
+                        }
+                        if (arithmetikchar[i] == '-'){
+                            floatdigit[i]=floatdigit[i]-floatdigit[i+1];
+                            System.out.print(" float "+i+" ="+floatdigit[i]+" ");
+                        }
+                        arithmetikchar = removeArrayElementChar(arithmetikchar,i);
+                        floatdigit = removeArrayElementFloat(floatdigit,i+1);
+                    }
+                }
+
+            }
+            System.out.println();
+            for(int i=0;i<floatdigit.length;i++){
+                System.out.print(" float="+floatdigit[i]+" ");
+            }
+            System.out.println();
+            for(int i=0;i<arithmetikchar.length;i++){
+                System.out.print(" arithmetic="+arithmetikchar[i]+" ");
+            }
 
         } else System.out.println("arithmetic signs bad");
+
+
+
+
 
     }
 }
